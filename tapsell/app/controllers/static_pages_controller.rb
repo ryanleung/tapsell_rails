2133ee98991f8c5680bc72e7b96c82cc3dd643ec
vincent <vincentvo@github.com>
2013-12-07 23:17:08 -0800

    Add static pages + Slim gem

---
+++ tapsell/app/controllers/static_pages_controller.rb
@@ -0,0 +1,34 @@
+class StaticPagesController < ApplicationController
+  def landing
+  end
+
+  def about
+  end
+
+  def team
+  end
+
+  def jobs
+  end
+
+  def blog
+  end
+
+  def contact
+  end
+
+  def faq
+  end
+
+  def buyer_faq
+  end
+
+  def terms
+  end
+
+  def privacy
+  end
+
+  def buyer_guarantee
+  end
+end
