9529a00dd65d3edcbc9447bbecaa803abf4aac93
vincent <vincentvo@github.com>
2013-12-08 17:33:36 -0800

    Add dyanmic page titles for static pages + formatting stuff

--- tapsell/app/helpers/static_pages_helper.rb
+++ tapsell/app/helpers/static_pages_helper.rb
@@ -1,2 +1,13 @@
 module StaticPagesHelper
+
+# Returns an appropriate dynamic title for static pages
+def full_title (page_title)
+	base_title = "Tapsell"
+	if page_title.empty?
+		base_title
+	else
+		"#{base_title} | #{page_title}"
+	end
+end
+
 end
