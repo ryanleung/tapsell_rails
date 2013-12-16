9ca1ed458dfa9b9a4e16deba5eca9167dcd116d9
vincent <vincentvo@github.com>
2013-12-16 05:37:28 -0800

    Authorization for user pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:show, :update]  
+  before_action :correct_user, only: [:show, :update]
+
   def new
   	@user = User.new
   end
@@ -21,7 +24,18 @@ class UsersController < ApplicationController
   def update
   end
 
+# Authorization methods for before filters
+# Remove hard-coded path later
 
+def signed_in_user
+  redirect_to '/sign-in', notice: "Please sign in." unless signed_in?
+end
+
+def correct_user
+  @user = User.find(params[:id])
+  redirect_to(root_url) unless current_user?(@user)
+end
+  
 private
 
   def user_params
