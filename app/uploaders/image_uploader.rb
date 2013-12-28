d016d8824ef2d2caa6954ed72259e8226b6c348c
Ryan Leung <ryanleung@berkeley.edu>
2013-12-28 14:01:07 -0800

    changed eager

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,12 +6,10 @@ class ImageUploader < CarrierWave::Uploader::Base
 	process :tags => ['post_image']
 
 	version :standard do
-		process :eager => true
 		process :resize_to_fill => [100, 150, :north]
 	end
 
 	version :thumbnail do
-		eager
 		resize_to_fit(50,50)
 	end
 
