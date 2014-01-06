bc1039f72f28ec0c4a0cc950905401145143e051
vincent <vincentvo@github.com>
2014-01-06 00:37:48 -0800

    Fix credit cards table

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,2 +1,8 @@
 class CreditCard < ActiveRecord::Base
+
+# Relationships - Ordered Alphabetically
+# --------------------------------------
+
+belongs_to :user
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140106081527) do
+ActiveRecord::Schema.define(version: 20140106083553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20140106081527) do
     t.datetime "updated_at"
     t.integer  "exp_month"
     t.integer  "exp_year"
+    t.integer  "user_id"
   end
 
   create_table "images", force: true do |t|
