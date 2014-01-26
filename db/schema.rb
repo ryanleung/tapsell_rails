923d6479f76bb0dfa7a31ff6603a36a0b1817ed5
Jacqueline Chen <jchen@Bound-JC.local>
2014-01-25 18:56:24 -0800

    add static pages

--- db/schema.rb
+++ db/schema.rb
@@ -97,13 +97,6 @@ ActiveRecord::Schema.define(version: 20140111070635) do
     t.datetime "updated_at"
   end
 
-
-  create_table "offers", force: true do |t|
-    t.integer  "listing_id"
-    t.integer  "offer_amount"
-    t.boolean  "accepted"
-  end
-
   create_table "message_chains", force: true do |t|
     t.integer  "buyer_id"
     t.integer  "seller_id"
@@ -123,12 +116,19 @@ ActiveRecord::Schema.define(version: 20140111070635) do
     t.datetime "updated_at"
   end
 
+  create_table "offers", force: true do |t|
+    t.integer  "listing_id"
+    t.integer  "offer_amount"
+    t.boolean  "accepted"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.integer  "rating"
     t.text     "bio"
-    t.text     "avatar_url"
     t.string   "password_hash"
     t.string   "email"
     t.string   "location"
