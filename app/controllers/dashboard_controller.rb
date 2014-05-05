9f05ea3550156f66df9966d055d97e5aa6abb2c6
Ryan Leung <ryanleung@berkeley.edu>
2014-05-04 18:34:22 -0700

    recent messages sorted correctly

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class DashboardController < ApplicationController
   def show
-    @recent_msg_chains = current_user.message_chains.try(:last, 5)
+    sorted_msg_chains = current_user.message_chains.try(:last, 5).sort_by &:created_at
+    @recent_msg_chains = sorted_msg_chains.reverse
     @active_listings = current_user.listings_as_seller.order("created_at DESC").try(:last, 5)
   end
 end
