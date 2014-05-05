85f10b35ea8f72cb7b56b8ffeb54e268c1b22026
Ryan Leung <ryanleung@berkeley.edu>
2014-05-04 18:27:51 -0700

    limit dashboard objects to 5

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def show
-    @recent_msg_chains = current_user.message_chains
-    @active_listings = current_user.listings_as_seller.order("created_at DESC")
+    @recent_msg_chains = current_user.message_chains.try(:last, 5)
+    @active_listings = current_user.listings_as_seller.order("created_at DESC").try(:last, 5)
   end
 end
