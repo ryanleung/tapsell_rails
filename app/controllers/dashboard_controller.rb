eeaea6fea69753fcf42a5c5e0b3844572c9ec1fc
Ryan Leung <ryanleung@berkeley.edu>
2014-05-10 01:55:02 -0700

    changed dashboard limit

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
   def show
-    sorted_msg_chains = current_user.message_chains.try(:last, 5).sort_by &:created_at
+    sorted_msg_chains = current_user.message_chains.try(:last, 3).sort_by &:created_at
     @recent_msg_chains = sorted_msg_chains.reverse
-    @active_listings = current_user.active_listings_as_seller.try(:last, 5)
+    @active_listings = current_user.active_listings_as_seller.try(:last, 3)
   end
 end
