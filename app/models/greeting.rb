3a2a8fd544e7aec08de57e18ecbb458d99fbb959
Ryan Leung <ryanleung@berkeley.edu>
2014-05-10 20:35:26 -0700

    whoops

--- app/models/greeting.rb
+++ app/models/greeting.rb
@@ -1,7 +1,7 @@
 class Greeting < ActiveRecord::Base
 
   def self.random_greeting
-    if Greeting.cout != 0
+    if Greeting.count != 0
       Greeting.all.sample.greeting_body
     else
       return "Hello there, happy #{Date.today.strftime("%A")}"
