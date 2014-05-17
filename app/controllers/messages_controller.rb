01a802b316ae433a823020595dfb1b814dd225e6
Ryan Leung <ryanleung@berkeley.edu>
2014-05-17 02:28:16 -0700

    header messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,6 +6,11 @@ class MessagesController < ApplicationController
     ordered_message_chains = @current_user.message_chains.sort_by &:updated_at
     @msg_chains = ordered_message_chains.reverse
     @current_msg_chain = @msg_chains[0]
+    if @current_msg_chain.seller == current_user
+      @other_user = @current_msg_chain.buyer
+    else
+      @other_user = @current_msg_chain.seller
+    end
     @greeting = Greeting.random_greeting
   end
 
@@ -19,8 +24,10 @@ class MessagesController < ApplicationController
     # mark the currently clicked message chain as read/clean
     if @current_msg_chain.seller == current_user
       @current_msg_chain.seller_dirty = false
+      @other_user = @current_msg_chain.buyer
     else
       @current_msg_chain.buyer_dirty = false
+      @other_user = @current_msg_chain.seller
     end
     @current_msg_chain.save!
   end
@@ -40,6 +47,12 @@ class MessagesController < ApplicationController
 
   def accept_offer_message
     message_chain = Message.find(params[:id].to_i).message_chain
+    offer = message_chain.offer
+    begin
+      Offer.accept_offer(current_user, offer)
+    rescue => e
+      raise "Accepting offer error: #{e.message}"
+    end
     MessageChain.send_message(current_user.id, message_chain.listing, "#{message_chain.seller.first_name} has accepted your offer!", Message::TYPE_DEFAULT,
       message_chain.id, message_chain.offer)
     redirect_to action: 'index'
