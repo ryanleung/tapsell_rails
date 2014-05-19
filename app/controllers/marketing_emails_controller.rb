b214291ab9d5f82e31b9fd20a7bd62d55331f1b9
Ryan Leung <ryanleung@berkeley.edu>
2014-05-19 11:00:54 -0700

    this week

--- app/controllers/marketing_emails_controller.rb
+++ app/controllers/marketing_emails_controller.rb
@@ -7,7 +7,7 @@ class MarketingEmailsController < ApplicationController
     @marketing_email = MarketingEmail.new(marketing_email_params)
     if @marketing_email.save
       redirect_to root_path
-      flash[:notice] = "RSVP successful!  You'll receive an email confirmation with further instructions shortly."
+      flash[:notice] = "RSVP successful!  You'll receive an email confirmation with further instructions this week."
     else
       redirect_to root_path
       flash[:notice] = "Oops!  There was a problem with the email you submitted.  Please double-check and try again."
