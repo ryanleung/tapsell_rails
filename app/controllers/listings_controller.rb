f4090751a6d86d3c4c2d9bd86290ee96b8e26bd3
Ryan Leung <ryanleung@berkeley.edu>
2014-05-27 01:57:38 -0700

    fixed removed listing bug

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -144,11 +144,11 @@ class ListingsController < ApplicationController
 private
 
   def order_listings(sort_detail)
-    @listings = Listing.all.order(sort_detail).page(params[:page]).limit(28)
+    @listings = Listing.where.not(status: Listing::STATUS_REMOVED).order(sort_detail).page(params[:page]).limit(28)
   end
 
   def order_by_category(name)
-    @listings = Listing.where(category: name).order("created_at DESC").page(params[:page]).limit(28)
+    @listings = Listing.where(category: name).where.not(status: Listing::STATUS_REMOVED).order("created_at DESC").page(params[:page]).limit(28)
   end
 
   def listing_params
