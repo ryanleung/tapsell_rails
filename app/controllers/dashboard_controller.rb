b854827fc1d33768ac04ad28215e1aa8db7cd3dc
Ryan Leung <ryanleung@berkeley.edu>
2014-06-02 21:43:15 -0700

    deactivated stuff

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class DashboardController < ApplicationController
+  before_action :signed_in_user
+
   def show
     sorted_msg_chains = current_user.message_chains.try(:last, 3).sort_by &:updated_at
     @recent_msg_chains = sorted_msg_chains.reverse
