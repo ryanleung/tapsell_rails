95d907e08a7fbe35dd04251f3726da04dced3e2e
Ryan Leung <ryanleung@berkeley.edu>
2014-06-02 21:51:20 -0700

    ugh deactivation sux

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,14 @@ class SessionsController < ApplicationController
   def create
   	user = User.authenticate(params[:session][:email].downcase, params[:session][:password])
   	if not user.nil?
-      flash.delete(:notice)
-      sign_in user
-      redirect_back_or dashboard_path(current_user.id)
-    elsif user.deactivated
-      flash[:notice] = 'This account has been deactivated, please contact team@tapsell.co to reactivate.'
-      render 'new'
+      if user.deactivated
+        flash[:notice] = 'This account has been deactivated, please contact team@tapsell.co to reactivate.'
+        render 'new'
+      else
+        flash.delete(:notice)
+        sign_in user
+        redirect_back_or dashboard_path(current_user.id)
+      end
     else
       flash[:notice] = 'That email / password combination is invalid, please try again.'
       render 'new'
